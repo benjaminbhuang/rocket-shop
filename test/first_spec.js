68c3ae3466242e9172c24b3ff166398f75b91b7d
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-09 23:01:17 -0400

    add subscription lib dir

--- test/first_spec.js
+++ test/first_spec.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 describe('A feature', function () {
     describe('A scenario', function () {
-        it('Should do something expected', function () {
+        it('Should do something unexpected!', function () {
             assert(true);
         });
     });
