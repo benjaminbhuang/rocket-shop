0e36ecbbc6039ff5b4008ac37e2c12012fa2ca23
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-09 21:35:28 -0400

    init commit

---
+++ test/first_spec.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+describe('A feature', function () {
+    describe('A scenario', function () {
+        it('Should do something expected', function () {
+            assert(true);
+        });
+    });
+    describe('Another scenario', function () {
+        it('Should do something expected again', function () {
+            assert(true);
+        });
+    });
+});
